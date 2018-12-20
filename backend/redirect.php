3189e54dbd8fe0efb50b18c4629dde71456e1f06
Laptop edo <gstv.qnd@outlook.com>
2018-12-20 01:35:35 -0300

    Conexion a ingreso funcionario

--- backend/redirect.php
+++ backend/redirect.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 
+/*
 if($_SESSION['estado']=="Admin"){
     header("Location: /i/adm/home.php");
 }else if($_SESSION['estado']=="Alumno"){
@@ -8,4 +9,21 @@ if($_SESSION['estado']=="Admin"){
 }else if($_SESSION['estado']=="Relator"){
     header("Location: /i/fnc/home.php");  
 } 
+*/
+
+switch ($_SESSION['estado']){
+    case "Admin":
+    header("Location: /i/adm/home.php");
+        break;
+    case "Alumno":
+    header("Location: /i/usr/home.php");
+        break;
+    case "Relator":
+    header("Location: /i/fnc/home.php");  
+        break;
+    case "Inactivo":
+    header("Location: error-ingreso.html");  
+        break;
+}
+
 ?>
